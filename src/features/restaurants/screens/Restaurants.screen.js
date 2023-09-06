d92235ce0a761a389ee8ca76fe94d5794e11a30e
Miguel Garcia <miguelangelgarciaalvizo@hotmail.com>
2023-09-06 12:27:24 -0600

    implement restaurant card list

--- src/features/restaurants/screens/Restaurants.screen.js
+++ src/features/restaurants/screens/Restaurants.screen.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import {
-    // StyleSheet,
-    StatusBar,
-    // Platform
-} from 'react-native';
+import { StatusBar } from 'react-native';
 import { Searchbar } from 'react-native-paper';
 import styled from 'styled-components/native';
 
+import Spacer from '../../../components/Spacer/Spacer.component';
 import RestaurantInfoCard from '../components/RestaurantInfoCard.component';
 
 const SafeArea = styled.SafeAreaView`
@@ -20,10 +17,13 @@ const SearchContainer = styled.View`
     padding: ${props => props.theme.SPACE[3]};
 `;
 
-const RestaurantListContainer = styled.View`
-    flex: 1;
-    padding: ${props => props.theme.SPACE[3]};
-`;
+// Gets access to the attributes of the FlatList
+const RestaurantList = styled.FlatList.attrs({
+    // These styles will be applied to the scroll view content container which wraps all of the child views
+    contentContainerStyle: {
+        padding: 16,
+    },
+})``;
 
 const Restaurants = () => {
     return (
@@ -31,13 +31,30 @@ const Restaurants = () => {
             <SearchContainer>
                 <Searchbar />
             </SearchContainer>
-            <RestaurantListContainer>
-                <RestaurantInfoCard />
-            </RestaurantListContainer>
+            <RestaurantList
+                data={[
+                    { name: 1 },
+                    { name: 2 },
+                    { name: 3 },
+                    { name: 4 },
+                    { name: 5 },
+                    { name: 6 },
+                    { name: 7 },
+                    { name: 8 },
+                    { name: 9 },
+                    { name: 10 },
+                ]}
+                keyExtractor={restaurant => restaurant.name}
+                renderItem={() => {
+                    return (
+                        <Spacer position="bottom" size="large">
+                            <RestaurantInfoCard />
+                        </Spacer>
+                    );
+                }}
+            />
         </SafeArea>
     );
 };
 
 export default Restaurants;
-
-// const styles = StyleSheet.create({});
